011a602c79424f2acfebb054e1b16eaaf1aad236
ishigo1987 <stephnguets@yahoo.fr>
2022-03-30 20:03:23 +0100

    Add product to cart

--- front/js/product.js
+++ front/js/product.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             return alert("Erreur lors de la récupération des données.");
 
         }
+
         // Ici tu affiches les informations du produit dans la page product.html
 
         document.querySelector(".item__img").innerHTML = `<img src=${response?.data?.imageUrl} alt=${response?.data?.altTxt} />`;
@@ -42,28 +43,97 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
 
         document.querySelector("#colors").insertAdjacentHTML("beforeend", colorsHTML);
+
+        addProductToBasket({ 
+            
+            urlId: urlId, 
+
+            name: response?.data?.name,
+            
+            imageSrc: response?.data?.imageUrl, 
+            
+            altxt: response?.data?.altTxt
+        });
    });
 
 
+   // Cette fonction ajoute un produite au panier.
 
-   
-// // *************************
+   function addProductToBasket(product){
 
-// const local = JSON.parse(localStorage.getItem("cart"));
-// if (local != null) {
+        document.querySelector("#addToCart").addEventListener("click", ()=>{
 
-//     console.log(local);
-// }
+            const productColor = document.querySelector("#colors").value;
 
-// addToCart.onclick = () => {
-// let cart = {
-//     quantity: quantity.value,
-//     colors: colors.value
-// };
-// localStorage.setItem("cart", JSON.stringify(cart))
+            const productQuantity = document.querySelector("#quantity").value;
 
-// }
+            if(productColor === "" || ["0", ""].includes(productQuantity) === true){
 
+                return alert("Veuillez choisir une couleur de canape et une quantite.");
+
+            }
+
+            const cart = localStorage.getItem("cart");
+
+            product.color = productColor;
+
+            product.quantity = productQuantity;
+
+            // In this case the localStorage is empty
+            if(cart === null){
+
+                 const cartArray = [];
+
+                 cartArray.push(product);
+
+                 return localStorage.setItem("cart", JSON.stringify(cartArray))    
+
+            }
+
+            const {name, color, quantity} = product;
+
+            // there the localStorage is not empty
+
+            const productsInfos = JSON.parse(cart);
+
+            // We need to check if the new product name is already inside the localStorage
+
+            const isInCart = productsInfos.some((productCart)=>{
+
+                    return productCart.name === name && productCart.color === color;
+
+            });
+
+           // In this case the product does not have the same name or color than the cart products
+
+           if(isInCart === false){
+
+                productsInfos.push(product);
+
+                return localStorage.setItem("cart", JSON.stringify(productsInfos));
+
+           }
+
+           // In this case the product is already exist inside the cart, we just need to update the product inside the cart
+
+           const updateCart = productsInfos.map((productCart)=>{
+
+                  if(productCart.color === color){
+
+                       productCart.quantity = Number(productCart.quantity) + Number(quantity);
+
+                  }
+
+                  return productCart;
+
+           });
+
+        
+           return localStorage.setItem("cart", JSON.stringify(updateCart));
+
+        });
+
+   }
 
 
 });
